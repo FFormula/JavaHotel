c87a80b396de8cdb2a15b78b4c729e497b731a27
Jevgenij Volosatov <fformula@gmail.com>
2018-05-21 14:50:19 +0300

    Updated

--- Room.java
+++ Room.java
@@ -15,30 +15,16 @@ public class Room
 		//
 	}
 	
-	@Override
-	public String toString()
+	public static Room find (String name)
 	{
 		//
-		return "";
+		return null;
 	}
 	
 	public int getUsedBeds ()
 	{
 		//
 		return 0;
-	}
-	
-	public static ArrayList<Room> getList() 
-	{
-		ArrayList<Room> list = new ArrayList<>();
-		//
-		return list;
-	}
-	
-	public static Room find (String name)
-	{
-		//
-		return null;
 	}
 	
 }
